cb1dd071fde52cb94e9aa9d4bffc8f1f2a29d894
mahdikohee <mahdikohee@gmail.com>
2025-05-29 17:12:06 +0600

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -17,3 +17,34 @@ void Modify_Array(int array[], int size) {
         array[i] *= 3;
     }
 }
+//same way 
+#include <iostream>
+using namespace std;
+
+double Average(int array[], int n);  // ফাংশনের ঘোষণা
+
+int main(void) {
+    int arr[] = {12, 32, 43, 54, 98, 43};
+
+    // sizeof(arr) মানে অ্যারের মোট সাইজ (bytes), আর একটি int এর সাইজ ভাগ দিলে উপাদান সংখ্যা পাওয়া যায়
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    double avg = Average(arr, size); // ফাংশন কল
+
+    cout << "The average number is " << avg << endl;
+
+    return 0;
+}
+
+double Average(int array[], int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += array[i];  // সব উপাদান যোগ হচ্ছে
+    }
+
+    return (double)sum / n;  // গড় রিটার্ন করা হচ্ছে
+}
+
+
+//
